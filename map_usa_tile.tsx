6add5383c9fb54a7115dcce5662f6ad1c5640342
skibidi Greg <grgr.gllrn@gmail.com>
2025-09-11 18:21:18 +0200

    creation map

---
+++ map_usa_tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="carte_us_pixel" tilewidth="1" tileheight="1" tilecount="375180" columns="780">
+ <image source="carte_us_pixel.png" width="780" height="481"/>
+</tileset>
