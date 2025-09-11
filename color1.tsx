04dd7431d5d3e7c1ea7d48598aee9132747fa3b6
skibidi Greg <grgr.gllrn@gmail.com>
2025-09-11 18:45:38 +0200

    change map
    change map

---
+++ color1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="green_color" tilewidth="1" tileheight="1" tilecount="115200" columns="400">
+ <image source="color1.png" width="400" height="288"/>
+</tileset>
