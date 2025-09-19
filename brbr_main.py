b2546d3733d806a0b80c62f1d1482e800ef92b8f
skibidi Greg <grgr.gllrn@gmail.com>
2025-09-19 13:49:20 +0200

    remplacement brbr main

--- brbr_main.py
+++ brbr_main.py
@@ -1,27 +1,56 @@
 import pygame, sys
+import pytmx
 from map import *
 
 class Simulation:
     def __init__(self) :
         pygame.init()
-        self.screen = pygame.display.set_mode((1600, 900))
+        self.screen = pygame.display.set_mode((1400, 800))
         self.clock = pygame.time.Clock()
         
-        self.map = Map()
-
+        self.infection = Infection()
+        self.map_image = pygame.image.load('carte_us_pixel.png')
+        self.map_image = pygame.transform.scale_by(self.map_image, 2).convert_alpha()  # x4
+        
+        self.tmx = pytmx.util_pygame.load_pygame('carte_usa.tmx')
 
     def draw_map(self) : 
-        cell_size = self.map.cell_size
-        
-        # for x in range(0,  self.map.width, cell_size):
-        #     pygame.draw.line(self.screen, (50, 50, 50), (x, 0), (x, self.map.height))
-        # for y in range(0, self.map.height, cell_size):
-        #     pygame.draw.line(self.screen, (50, 50, 50), (0, y), (self.map.width, y))
+        cell_size = self.infection.cell_size
             
-        for px_pos in self.map.infected_pixels_pos :
-            pygame.draw.rect(self.screen, (255, 0, 0), pygame.Rect(px_pos[0], px_pos[1], cell_size, cell_size))
-        for px in self.map.dead_pixels_pos :
-            pygame.draw.rect(self.screen, (128, 128, 128), pygame.Rect(px[0], px[1], cell_size, cell_size))
+        for pos in self.infection.infected_pixels_pos :
+            pygame.draw.rect(self.screen, (255, 0, 0), pygame.Rect(pos[0], pos[1], cell_size, cell_size))
+        for p in self.infection.dead_pixels_pos :
+            pygame.draw.rect(self.screen, (128, 128, 128), pygame.Rect(p[0], p[1], cell_size, cell_size))
+
+
+    # def debug_layer(self, layer_name):
+    #     layer = self.tmx.get_layer_by_name(layer_name)
+    #     total = 0
+    #     non_empty = 0
+    #     gids = {}
+    #     for x, y, gid in layer:
+    #         total += 1
+    #         if gid:
+    #             non_empty += 1
+    #             gids[gid] = gids.get(gid, 0) + 1
+    #     print(f"[{layer_name}] cases totales: {total}, tuiles posées: {non_empty}")
+    #     # Top 10 GIDs les plus fréquents
+    #     top = sorted(gids.items(), key=lambda kv: kv[1], reverse=True)[:10]
+    #     print("Top GIDs:", top)
+
+
+    # def draw_tile_top_left_pixels(self, layer_name):
+    #     tw, th = self.tmx.tilewidth, self.tmx.tileheight
+    #     layer = self.tmx.get_layer_by_name(layer_name)
+
+    #     # IMPORTANT : on saute les cases vides (gid == 0)
+    #     for x, y, gid in layer:
+    #         if gid == 0:
+    #             continue
+    #         px = x * tw
+    #         py = y * th
+    #         pygame.draw.rect(self.screen, (0, 0, 0), pygame.Rect(px, py, 1, 1))
+
         
         
     def run(self) :
@@ -32,9 +61,11 @@ class Simulation:
                     pygame.quit()
                     sys.exit()
 
-            self.screen.fill("black")
+            self.screen.fill("white")
             self.draw_map()
-            self.map.update_infection()
+            # self.debug_layer('frontieres')
+            # self.draw_tile_top_left_pixels('frontieres')
+            self.infection.update_infection()
             pygame.display.update()
 
 
