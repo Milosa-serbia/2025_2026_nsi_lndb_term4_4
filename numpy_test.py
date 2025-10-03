797884bbea2254f0e7e65eadf1ff4462f5c494c2
Roman-milos <romanmilos8@gmail.com>
2025-10-03 15:18:58 +0200

    on peut sauvegarder la map mtn

--- numpy_test.py
+++ numpy_test.py
@@ -5,7 +5,7 @@ WIDTH = 1400
 HEIGHT = 800
 
 
-def grid_to_surf(gray_2d, surf) :
+def grid_to_surf(gray_2d, surf):
     # Étend (H,W) -> (H,W,1) -> (H,W,3)
     rgb = np.repeat(gray_2d[:, :, None], 3, axis=2)      # (H, W, 3)
     rgb = np.transpose(rgb, (1, 0, 2))                   # (W, H, 3) pour Pygame
@@ -13,42 +13,46 @@ def grid_to_surf(gray_2d, surf) :
     return surf
 
 
-def main() :
+def save_grid(grid, filename="dessin.npy"):
+    np.save(filename, grid)
+    print(f"Tableau NumPy sauvegardé sous {filename}")
+
+
+def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    grid  = np.zeros((HEIGHT, WIDTH), dtype=np.uint8)
-    print(grid)
+    grid  = np.load('dessin.npy')
     clock = pygame.time.Clock()
     surf = pygame.Surface((WIDTH, HEIGHT))
     drawing = False
     
     running = True
-    while running :
+    while running:
         
-        for event in pygame.event.get() :
-            if event.type == pygame.QUIT : 
-                pygame.quit()
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 drawing = True
-            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1 : 
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1: 
                 drawing = False
-        
-        if drawing : 
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:   # touche "s" pour sauvegarder
+                    save_grid(grid)
+                    
+                if event.key == pygame.K_z :
+                    grid = np.zeros((HEIGHT, WIDTH), dtype=np.uint8)
+
+        if drawing: 
             x, y = pygame.mouse.get_pos()
-            if 0 - 1<= x <= WIDTH - 1 and 0 - 1<= y <= HEIGHT - 1 :
-                grid[y, x] = 255
-                grid[y + 1, x] = 255
-                grid[y, x + 1] = 255
-                grid[y + 1, x + 1] = 255
-                grid[y + 2, x] = 255
-                grid[y, x + 2] = 255
-                grid[y + 2, x + 2] = 255
-                grid[y + 1, x + 2] = 255
-                grid[y + 2, x + 1] = 255
+            if 0 <= x < WIDTH - 2 and 0 <= y < HEIGHT - 2:
+                grid[y:y+5, x:x+5] = 255  # petit carré de 3x3
 
         surf = grid_to_surf(grid, surf)
         screen.blit(surf, (0, 0))
         pygame.display.flip()
         clock.tick(1000)  
+    
+    pygame.quit()
                 
 main()
